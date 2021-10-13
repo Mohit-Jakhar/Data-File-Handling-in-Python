4ad4f39edbc450278a7b7a205389ec7edc5afd52
Mohit Jakhar <87889989+Mohit-Jakhar@users.noreply.github.com>
2021-10-13 19:50:06 +0530

    Create test.py

---
+++ Adding data to an existing file/test.py
@@ -0,0 +1,7 @@
+f=open(“Sample.txt”, ’a’)	    #Opening file in append mode
+f.write(“you are working on Python 3.7”)	
+f.write(“Simple syntax of the language \n”)
+f.write(“Makes python easy to read and write”)
+print(“More data is appended to the file”)
+f.close()
+
